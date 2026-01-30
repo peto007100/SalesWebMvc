c6ee176bdc82f84b60f01fd6024d5def42acc024
Vinicio <vn21052005@gmail.com>
2026-01-30 02:17:01 -0300

    SeedingService

--- Program.cs
+++ Program.cs
@@ -17,6 +17,8 @@ builder.Services.AddDbContext<SalesWebMvcContext>(options =>
     )
 );
 
+builder.Services.AddScoped<SeedingService>();
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -28,6 +30,16 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    // Aquele método Configure não existe mais como uma função separada.
+    // Para rodar o Seed no Program.cs moderno, fazemos assim:
+    using (var scope = app.Services.CreateScope())
+    {
+        var seedingService = scope.ServiceProvider.GetRequiredService<SeedingService>();
+        seedingService.Seed();
+    }
 }
 
 app.UseHttpsRedirection();
